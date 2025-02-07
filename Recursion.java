56de04480813955d0eb0bd36269b98f03f218f60
shubham jangra <146321989+shubhamjangra-1234@users.noreply.github.com>
2025-02-07 12:48:32 +0530

    [2nd commit]

--- Recursion.java
+++ Recursion.java
@@ -12,14 +12,77 @@
 //     }
 // }
 
+// public class Recursion{
+//     public static void Number(int  num){
+//         if(num == 100 ){
+//             return;
+//         }
+//         System.out.println(num);
+//         Number(num+1);
+//     }
+//     public static void main(String args[]){
+//         int num = 0;
+//         Number(num);
+//     }
+// } 
+//print sum if n natural numbers
+// public class Recursion{
+//     public static void Number(int a , int b, int Sum){
+//         if(a == b){
+//             Sum += a;
+//             System.out.println(Sum);
+//             return;
+//         }
+//         Sum += a;
+//         Number(a+1 , b , Sum);
+//     }
+//     public static void main(String[] args) {
+//         Number(0, 50, 0);
+//     }
+
+// }
+//print factorial of n numbers
+// public class Recursion{
+//     public static int Factor(int n){
+//         if (n == 0 || n == 1 ){
+//             return 1;
+//         }
+//         int a = Factor(n-1);
+//         int b = n*a;
+//         return b;
+//     }
+//     public static void main(String[] args) {
+//         int n = 5;
+//         System.out.println(Factor(n));
+//     }
+// }
+// public class Recursion{
+//     public static int Factor(int n ){
+//         if(n==1){
+//             return 1;
+//         }
+//         int a = Factor(n-1);
+//         int b = a * n;
+//         return b ;
+//     }
+//     public static void main(String args[]){
+//         int n = 40;
+//         System.out.println(Factor(4));
+//     }
+// }
 public class Recursion{
-    public static int total(int a, int b){
-       int sum = a+b;
-       return sum;
+    public static int Factor(int n ){
+        if( n == 1){
+            return 1;
+        }
+        int A = Factor(n-1);
+        int B = n*A;
+        
+        return B;
     }
-    public static void main(String[] args) {
-        int a = 90;
-        int b = 10;
-        System.out.println("the sum of"+a+"+"+b+"is"+total(a, b));
+    public static void main(String args[]){
+        int n = 5;
+        System.out.print( Factor(n));
+        
     }
 }
--- Sorting.java
+++ Sorting.java
@@ -8,10 +8,9 @@
 //     }
 //     public static void main(String args[]){
 //         int Arr[] = {7,4,8,3,1,0};
-
-//         for (int i = 0; i<Arr.length; i++){
+//         for (int i = 0; i<Arr.length-1; i++){
 //             for(int j=0 ; j<Arr.length-i-1; j++){
-//                 if(Arr[j] < Arr[j+1]){
+//                 if(Arr[j] > Arr[j+1]){
 //                     int Temp = Arr[j];
 //                     Arr[j] = Arr[j+1];
 //                     Arr[j+1] = Temp;
@@ -39,10 +38,42 @@
 //                 int temp = arr[smallest];
 //                 arr[smallest] = arr[i];
 //                 arr[i] = temp  ;
-
 //             }
-
 //         }
 //         Sort(arr);
 //     }
 // } 
+import java.util.Scanner;
+
+public class Sorting {
+
+    public static void Sorted(int Unsort[]) {
+        System.out.println("this is your sorted array");
+        for (int i = 0; i <Unsort.length; i++) {
+            System.out.print(Unsort[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the length for you array");
+        int user = sc.nextInt();
+        int Unsort[] = new int[user];
+        System.out.println("enter the number in your array");
+        for (int i = 0; i < user; i++) {
+            Unsort[i] = sc.nextInt();
+        }
+        
+        for (int i = 0; i <user - 1; i++) {
+            for (int j = 0; j< user - i - 1; j++) {
+                if (Unsort[j] > Unsort[j + 1]) {
+                    int temp = Unsort[j];
+                    Unsort[j] = Unsort[j + 1];
+                    Unsort[j + 1] = temp;
+                }
+            }
+            Sorted(Unsort);
+        }
+    }
+}
